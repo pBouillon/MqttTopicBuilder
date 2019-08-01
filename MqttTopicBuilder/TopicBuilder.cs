56233236bab8821cbc9159247aafc23163a5a72e
pbouillon <pro.pierre.bouillon@protonmail.com>
2019-08-01 06:39:35 +0200

    WiP builder's logic

--- MqttTopicBuilder/TopicBuilder.cs
+++ MqttTopicBuilder/TopicBuilder.cs
@@ -12,7 +12,7 @@
 namespace MqttTopicBuilder
 {
     using MqttUtils;
-    using System.Collections;
+    using System.Collections.Generic;
 
     /// <summary>
     /// TODO: doc
@@ -22,30 +22,51 @@ namespace MqttTopicBuilder
         /// <summary>
         /// TODO: doc
         /// </summary>
+        private bool isAppendingBlocked
+            => StagedTopics
+                .Peek()
+                .Equals(Wildcards.MultiLevel.ToString());
+
+        /// <summary>
+        /// TODO: doc
+        /// </summary>
         public int Level
             => StagedTopics.Count;
 
         /// <summary>
         /// TODO: doc
         /// </summary>
-        public Queue StagedTopics { get; }
+        public Queue<string> StagedTopics { get; }
 
         /// <summary>
         /// TODO: doc
         /// </summary>
+        /// <param name="maxLength"></param>
         // TODO: str initialization
         public TopicBuilder(int maxLength = Topics.MaxLength)
         {
-            StagedTopics = new Queue();
+            StagedTopics = new Queue<string>(maxLength);
         }
 
         /// <summary>
         /// TODO: doc
         /// </summary>
+        /// <param name="topic"></param>
         /// <returns></returns>
         public TopicBuilder AddTopic(string topic)
         {
-            // TODO
+            CheckAppendingAllowance();
+
+            // Manually adding separators is forbidden
+            if (topic.Contains(Topics.Separator))
+            {
+                // TODO: raise exception
+            }
+
+            // TODO: check for patterns as '##', '#+', etc.
+
+            StagedTopics.Enqueue(topic);
+
             return this;
         }
 
@@ -55,7 +76,11 @@ namespace MqttTopicBuilder
         /// <returns></returns>
         public TopicBuilder AddWildcardSingleLevel()
         {
-            // TODO
+            CheckAppendingAllowance();
+
+            StagedTopics.Enqueue(
+                Wildcards.SingleLevel.ToString());
+
             return this;
         }
 
@@ -65,8 +90,37 @@ namespace MqttTopicBuilder
         /// <returns></returns>
         public TopicBuilder AddWildcardMultiLevel()
         {
-            // TODO
+            CheckAppendingAllowance();
+
+            StagedTopics.Enqueue(
+                Wildcards.MultiLevel.ToString());
+
             return this;
+        }
+
+        /// <summary>
+        /// TODO: doc
+        /// </summary>
+        /// <returns></returns>
+        public Topic Build()
+        {
+            // An empty builder will result in the construction of the smallest topic possible
+            var generatedTopic = StagedTopics.Count == 0
+                ? Topics.Separator.ToString()
+                : string.Join(Topics.Separator, StagedTopics);
+
+            return new Topic(generatedTopic);
+        }
+
+        /// <summary>
+        /// TODO: doc
+        /// </summary>
+        private void CheckAppendingAllowance()
+        {
+            if (!isAppendingBlocked)
+            {
+                // TODO: raise exception
+            }
         }
     }
 }
