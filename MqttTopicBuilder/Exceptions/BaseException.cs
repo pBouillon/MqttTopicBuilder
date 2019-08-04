aa3932655f1faaebabd161535eb5b3427d7d4e86
pBouillon <pro.pierre.bouillon@protonmail.com>
2019-08-04 15:29:25 +0200

    Add custom exception structure

---
+++ MqttTopicBuilder/Exceptions/BaseException.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      MqttTopicBuilder - https://github.com/pBouillon/MqttTopicBuilder
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/MqttTopicBuilder/blob/master/LICENSE
+ */
+
+namespace MqttTopicBuilder.Exceptions
+{
+    using System;
+
+    public class BaseException : Exception
+    {
+        public BaseException() { }
+
+        public BaseException(string message)
+            : base(message) { }
+
+        public BaseException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
