d524dea210885ebdbe42e674e603241ec7552448
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2020-07-06 18:17:47 +0200

    📚 Add missing documentation

--- MqttTopicBuilder.UnitTests/Validators/Rules/RawTopicRules/MustBeUtf8UnitTest.cs
+++ MqttTopicBuilder.UnitTests/Validators/Rules/RawTopicRules/MustBeUtf8UnitTest.cs
@@ -23,6 +23,9 @@ namespace MqttTopicBuilder.UnitTests.Validators.Rules.RawTopicRules
     /// </summary>
     public class MustBeUtf8UnitTest
     {
+        /// <summary>
+        /// Ensure that a raw topic not encoded with UTF-8 will not pass the rule
+        /// </summary>
         [Fact]
         public void Validate_OnNonUtf8Topic()
         {
@@ -39,6 +42,9 @@ namespace MqttTopicBuilder.UnitTests.Validators.Rules.RawTopicRules
                 .Throw<InvalidTopicException>("because this raw topic is not UTF-8");
         }
 
+        /// <summary>
+        /// Ensure that a raw topic encoded with UTF-8 will pass the rule
+        /// </summary>
         [Fact]
         public void Validate_OnUtf8Topic()
         {
