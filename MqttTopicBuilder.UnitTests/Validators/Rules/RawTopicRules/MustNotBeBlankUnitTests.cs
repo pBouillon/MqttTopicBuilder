0857cff4bb0b35cb3e29fa7a80fc38940d2707bf
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2020-07-07 07:21:06 +0200

    ✅ Add MustNotBeBlank unit tests

---
+++ MqttTopicBuilder.UnitTests/Validators/Rules/RawTopicRules/MustNotBeBlankUnitTests.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      MqttTopicBuilder - https://github.com/pBouillon/MqttTopicBuilder
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/MqttTopicBuilder/blob/master/LICENSE
+ */
+
+using System;
+using AutoFixture;
+using FluentAssertions;
+using MqttTopicBuilder.Exceptions.Classes;
+using MqttTopicBuilder.UnitTests.Utils;
+using MqttTopicBuilder.Validators.Rules.RawTopicRules;
+using Xunit;
+
+namespace MqttTopicBuilder.UnitTests.Validators.Rules.RawTopicRules
+{
+    /// <summary>
+    /// Unit test suite for <see cref="MustNotBeBlank"/>
+    /// </summary>
+    public class MustNotBeBlankUnitTests
+    {
+        /// <summary>
+        /// Private instance of <see cref="IFixture"/> for test data generation purposes
+        /// </summary>
+        private static readonly IFixture Fixture = new Fixture();
+
+        [Fact]
+        public void Validate_OnBlankString()
+        {
+            // Arrange
+            var whitespaceCount = Fixture.Create<int>() + 1;
+            var rawTopic = new string(' ', whitespaceCount);
+
+            var rule = new MustNotBeBlank();
+
+            // Act
+            Action validatingRawTopic = () =>
+                rule.Validate(rawTopic);
+
+            // Assert
+            validatingRawTopic.Should()
+                .Throw<EmptyTopicException>(
+                    "because a blank topic should break the rule");
+        }
+
+        [Fact]
+        public void Validate_OnEmptyString()
+        {
+            // Arrange
+            var rawTopic = string.Empty;
+
+            var rule = new MustNotBeBlank();
+
+            // Act
+            Action validatingRawTopic = () =>
+                rule.Validate(rawTopic);
+
+            // Assert
+            validatingRawTopic.Should()
+                .Throw<EmptyTopicException>(
+                    "because an empty topic should break the rule");
+        }
+
+        [Fact]
+        public void Validate_OnNonEmptyString()
+        {
+            // Arrange
+            var rawTopic = TestUtils.GenerateSingleValidTopic();
+            var rule = new MustNotBeBlank();
+
+            // Act
+            Action validatingRawTopic = () =>
+                rule.Validate(rawTopic);
+
+            // Assert
+            validatingRawTopic.Should()
+                .NotThrow<EmptyTopicException>(
+                    "because a topic with a valid value should not break the rule");
+        }
+
+        [Fact]
+        public void Validate_OnNullString()
+        {
+            // Arrange
+            string rawTopic = null;
+
+            var rule = new MustNotBeBlank();
+
+            // Act
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Action validatingRawTopic = () =>
+                rule.Validate(rawTopic);
+
+            // Assert
+            validatingRawTopic.Should()
+                .Throw<EmptyTopicException>(
+                    "because an null topic should break the rule");
+        }
+    }
+}
