7ac7b45ed227c80fc3a6643e0262c8ba0d05312c
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2020-07-14 12:38:14 +0200

    ✨ Create builder states

---
+++ MqttTopicBuilder/Builder/BuilderState/PublisherState.cs
@@ -0,0 +1,70 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      MqttTopicBuilder - https://github.com/pBouillon/MqttTopicBuilder
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/MqttTopicBuilder/blob/master/LICENSE
+ */
+
+using MqttTopicBuilder.Exceptions.Classes;
+using MqttTopicBuilder.Validators;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MqttTopicBuilder.Builder.BuilderState
+{
+    /// <summary>
+    /// Specialized <see cref="IBuilderState"/> for topic construction
+    /// when the consumer is <see cref="TopicConsumer.Publisher"/>
+    /// </summary>
+    public class PublisherState : BuilderState
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="topicBuilder">State context</param>
+        public PublisherState(ITopicBuilder topicBuilder) 
+            : base(topicBuilder) { }
+
+        /// <inheritdoc cref="BuilderState.AddMultiLevelWildcard"/>
+        public override ITopicBuilder AddMultiLevelWildcard()
+            => throw new IllegalStateOperationException(
+                "multi-level wildcard are forbidden when publishing");
+
+        /// <inheritdoc cref="BuilderState.AddTopic"/>
+        public override ITopicBuilder AddTopic(string topic)
+            => AddTopics(topic);
+        
+        /// <inheritdoc cref="BuilderState.AddTopics(IEnumerable&lt;string&gt;)"/>
+        public override ITopicBuilder AddTopics(IEnumerable<string> topics)
+        {
+            var validator = ValidatorFactory.GetPublishedTopicValidator();
+         
+            // Enumerate topics to avoid multiple enumerations
+            var enumeratedTopics = topics.ToList();
+
+            // Validate each one of the topics
+            enumeratedTopics.ForEach(_ => 
+                validator.Validate(_));
+
+            // Append all topics
+            var modifiedCollection = TopicBuilder.TopicCollection
+                .AddTopics(enumeratedTopics);
+            
+            // Return the new instance of the builder with the new topics
+            return new TopicBuilder(modifiedCollection, TopicBuilder.Consumer);
+        }
+
+        /// <inheritdoc cref="BuilderState.AddTopics(string[])"/>
+        public override ITopicBuilder AddTopics(params string[] topics)
+            => AddTopics((IEnumerable<string>) topics);
+
+        /// <inheritdoc cref="BuilderState.AddSingleLevelWildcard"/>
+        public override ITopicBuilder AddSingleLevelWildcard()
+            => throw new IllegalStateOperationException(
+                "single-level wildcard are forbidden when publishing");
+    }
+}
---
+++ MqttTopicBuilder/Builder/BuilderState/SubscriberState.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      MqttTopicBuilder - https://github.com/pBouillon/MqttTopicBuilder
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/MqttTopicBuilder/blob/master/LICENSE
+ */
+
+using System.Collections.Generic;
+
+namespace MqttTopicBuilder.Builder.BuilderState
+{
+    /// <summary>
+    /// Specialized <see cref="IBuilderState"/> for topic construction
+    /// when the consumer is <see cref="TopicConsumer.Subscriber"/>
+    /// </summary>
+    public class SubscriberState : BuilderState
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="topicBuilder">State context</param>
+        public SubscriberState(ITopicBuilder topicBuilder) 
+            : base(topicBuilder) { }
+
+        /// <inheritdoc cref="BuilderState.AddMultiLevelWildcard"/>
+        public override ITopicBuilder AddMultiLevelWildcard()
+        {
+            var modifiedCollection = TopicBuilder.TopicCollection.AddMultiLevelWildcard();
+            return new TopicBuilder(modifiedCollection, TopicBuilder.Consumer);
+        }
+
+        /// <inheritdoc cref="BuilderState.AddTopic"/>
+        public override ITopicBuilder AddTopic(string topic)
+            => AddTopics(topic);
+
+        /// <inheritdoc cref="BuilderState.AddTopics(IEnumerable&lt;string&gt;)"/>
+        public override ITopicBuilder AddTopics(IEnumerable<string> topics)
+        {
+            var modifiedCollection = TopicBuilder.TopicCollection.AddTopics(topics);
+            return new TopicBuilder(modifiedCollection, TopicBuilder.Consumer);
+        }
+
+        /// <inheritdoc cref="BuilderState.AddTopics(string[])"/>
+        public override ITopicBuilder AddTopics(params string[] topics)
+            => AddTopics((IEnumerable<string>) topics);
+
+        /// <inheritdoc cref="BuilderState.AddSingleLevelWildcard"/>
+        public override ITopicBuilder AddSingleLevelWildcard()
+        {
+            var modifiedCollection = TopicBuilder.TopicCollection.AddSingleLevelWildcard();
+            return new TopicBuilder(modifiedCollection, TopicBuilder.Consumer);
+        }
+    }
+}
