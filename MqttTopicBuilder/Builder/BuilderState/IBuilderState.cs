200480f36a94bf3f7775f8eec1ec6db299bde7c3
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2020-07-14 12:37:55 +0200

    ✨ Create interface for builder state pattern

---
+++ MqttTopicBuilder/Builder/BuilderState/IBuilderState.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      MqttTopicBuilder - https://github.com/pBouillon/MqttTopicBuilder
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/MqttTopicBuilder/blob/master/LICENSE
+ */
+
+using System.Collections.Generic;
+
+namespace MqttTopicBuilder.Builder.BuilderState
+{
+    /// <summary>
+    /// <see cref="ITopicBuilder"/> inner state for method's behavior
+    /// modifications
+    /// </summary>
+    public interface IBuilderState
+    {
+        /// <summary>
+        /// Add a multi-level wildcard to the builder
+        /// </summary>
+        /// <returns>An instance of <see cref="ITopicBuilder"/> holding the appended wildcard</returns>
+        ITopicBuilder AddMultiLevelWildcard();
+
+        /// <summary>
+        /// Add a topic to the builder
+        /// </summary>
+        /// <param name="topic">Topic to be added</param>
+        /// <returns>An instance of <see cref="ITopicBuilder"/> holding the appended topic</returns>
+        ITopicBuilder AddTopic(string topic);
+
+        /// <summary>
+        /// Add several topics to the builder
+        /// </summary>
+        /// <param name="topics">Topics to be added</param>
+        /// <returns>An instance of <see cref="ITopicBuilder"/> holding the appended topics</returns>
+        ITopicBuilder AddTopics(IEnumerable<string> topics);
+
+        /// <summary>
+        /// Add a multi-level wildcard to the builder
+        /// </summary>
+        /// <param name="topics">Topics to be added</param>
+        /// <returns>An instance of <see cref="ITopicBuilder"/> holding the appended topic</returns>
+        ITopicBuilder AddTopics(params string[] topics);
+
+        /// <summary>
+        /// Add a single-level wildcard to the builder
+        /// </summary>
+        /// <returns>An instance of <see cref="ITopicBuilder"/> holding the appended wildcard</returns>
+        ITopicBuilder AddSingleLevelWildcard();
+    }
+}
