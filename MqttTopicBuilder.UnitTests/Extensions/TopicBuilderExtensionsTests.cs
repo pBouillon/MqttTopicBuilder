e5da613e12fbebd5e1a1081a0a6590c77ccfd5ec
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-08-15 15:22:08 +0200

    ✅ Add ToSubscriberBuilder unit tests

--- MqttTopicBuilder.UnitTests/Extensions/TopicBuilderExtensionsTests.cs
+++ MqttTopicBuilder.UnitTests/Extensions/TopicBuilderExtensionsTests.cs
@@ -9,7 +9,13 @@
  *      MIT - https://github.com/pBouillon/MqttTopicBuilder/blob/master/LICENSE
  */
 
+using System;
+using System.Collections.Generic;
+using AutoFixture;
+using FluentAssertions;
 using MqttTopicBuilder.Builder;
+using MqttTopicBuilder.Constants;
+using MqttTopicBuilder.Exceptions.Classes;
 using MqttTopicBuilder.Extensions;
 using Xunit;
 
@@ -21,17 +27,74 @@ namespace MqttTopicBuilder.UnitTests.Extensions
     public class TopicBuilderExtensionsTests
     {
         /// <summary>
+        /// Private instance of <see cref="IFixture"/> for test data generation purposes
+        /// </summary>
+        private static readonly IFixture Fixture = new Fixture();
+
+        /// <summary>
         /// Ensure the behavior of a conversion of a <see cref="ITopicBuilder"/> whose consumer is
         /// <see cref="TopicConsumer.Publisher"/>
         /// </summary>
         [Fact]
-        public void ToSubscriberBuilder_FromPublisherBuilder() { }
+        public void ToSubscriberBuilder_FromPublisherBuilder()
+        {
+            // Arrange
+            var topics = Fixture.Create<List<string>>();
+
+            var publisherBuilder = new TopicBuilder(topics.Count + 1, TopicConsumer.Publisher)
+                .AddTopics(topics);
+
+            // Act
+            var subscriberBuilder = publisherBuilder.ToSubscriberBuilder();
+
+            // Assert
+            subscriberBuilder.Levels
+                .Should()
+                .Be(publisherBuilder.Levels,
+                    "because the content of the builder should remain the same");
+
+            subscriberBuilder.TopicCollection.ToArray()
+                .Should()
+                .Contain(publisherBuilder.TopicCollection.ToArray(),
+                    "because the content of the builder should remain the same");
+
+            subscriberBuilder.Consumer
+                .Should()
+                .Be(TopicConsumer.Subscriber,
+                    "because the consumer should have changed for the converted builder");
+        }
 
         /// <summary>
         /// Ensure the behavior of a conversion of a <see cref="ITopicBuilder"/> whose consumer is
         /// <see cref="TopicConsumer.Subscriber"/>
         /// </summary>
         [Fact]
-        public void ToSubscriberBuilder_FromSubscriberBuilder() { }
+        public void ToSubscriberBuilder_FromSubscriberBuilder()
+        {
+            // Arrange
+            var topics = Fixture.Create<List<string>>();
+
+            var builder = new TopicBuilder(topics.Count + 1, TopicConsumer.Subscriber)
+                .AddTopics(topics);
+
+            // Act
+            var subscriberBuilder = builder.ToSubscriberBuilder();
+
+            // Assert
+            subscriberBuilder.Levels
+                .Should()
+                .Be(builder.Levels,
+                    "because the content of the builder should remain the same");
+
+            subscriberBuilder.TopicCollection.ToArray()
+                .Should()
+                .Contain(builder.TopicCollection.ToArray(),
+                    "because the content of the builder should remain the same");
+
+            subscriberBuilder.Consumer
+                .Should()
+                .Be(TopicConsumer.Subscriber,
+                    "because the consumer should have changed for the converted builder");
+        }
     }
 }
