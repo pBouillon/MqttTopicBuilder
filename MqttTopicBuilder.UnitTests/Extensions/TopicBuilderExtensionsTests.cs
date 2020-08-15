6cbab495c2dbf55aec526b8516e2a5e854fa3554
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-08-15 12:14:40 +0200

    🌱 Create builder extensions unit test file

---
+++ MqttTopicBuilder.UnitTests/Extensions/TopicBuilderExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Author
+ *      Pierre Bouillon - https://github.com/pBouillon
+ *
+ * Repository
+ *      MqttTopicBuilder - https://github.com/pBouillon/MqttTopicBuilder
+ *
+ * License
+ *      MIT - https://github.com/pBouillon/MqttTopicBuilder/blob/master/LICENSE
+ */
+
+using MqttTopicBuilder.Extensions;
+
+namespace MqttTopicBuilder.UnitTests.Extensions
+{
+    /// <summary>
+    /// Unit test suite for <see cref="TopicBuilderExtensions"/>
+    /// </summary>
+    public class TopicBuilderExtensionsTests
+    {
+        
+    }
+}
