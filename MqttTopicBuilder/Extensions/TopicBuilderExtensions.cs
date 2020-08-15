3d37f1a76f196c53e4426b7ec7f352f4726ea5ce
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-08-15 15:48:29 +0200

    ✨ Add generic conversion method and ToPublisherBuilder extension method

--- MqttTopicBuilder/Extensions/TopicBuilderExtensions.cs
+++ MqttTopicBuilder/Extensions/TopicBuilderExtensions.cs
@@ -19,6 +19,19 @@ namespace MqttTopicBuilder.Extensions
     public static class TopicBuilderExtensions
     {
         /// <summary>
+        /// Get an instance of a <see cref="ITopicBuilder"/> in <see cref="TopicConsumer.Publisher"/> mode
+        /// from an existing builder
+        /// </summary>
+        /// <param name="builder">Builder to convert</param>
+        /// <returns>A new builder whose consumer is <see cref="TopicConsumer.Publisher"/></returns>
+        /// <remarks>
+        /// If the builder's consumer already is <see cref="TopicConsumer.Publisher"/>, this will return a clone
+        /// of the provided <paramref name="builder"/>
+        /// </remarks>
+        public static ITopicBuilder ToPublisherBuilder(this ITopicBuilder builder)
+            => Convert(builder, TopicConsumer.Publisher);
+
+        /// <summary>
         /// Get an instance of a <see cref="ITopicBuilder"/> in <see cref="TopicConsumer.Subscriber"/> mode
         /// from an existing builder
         /// </summary>
@@ -29,10 +42,22 @@ namespace MqttTopicBuilder.Extensions
         /// of the provided <paramref name="builder"/>
         /// </remarks>
         public static ITopicBuilder ToSubscriberBuilder(this ITopicBuilder builder)
-            => builder.Consumer == TopicConsumer.Subscriber
-                // If the builder is already in the right state, return its clone
-                ? builder.Clone() 
-                // ReSharper disable once RedundantArgumentDefaultValue
-                : new TopicBuilder(builder.TopicCollection, TopicConsumer.Subscriber);
+            => Convert(builder, TopicConsumer.Subscriber);
+
+        /// <summary>
+        /// Get an instance of a <see cref="ITopicBuilder"/> in the desired <paramref name="target"/> mode
+        /// from an existing builder
+        /// </summary>
+        /// <param name="builder">Builder to convert</param>
+        /// <param name="target">Mode in which converting the target</param>
+        /// <returns>A new builder whose consumer is <paramref name="target"/></returns>
+        /// <remarks>
+        /// If the builder's consumer already is in <paramref name="target"/> mode, this will return a clone
+        /// of the provided <paramref name="builder"/>
+        /// </remarks>
+        private static ITopicBuilder Convert(ITopicBuilder builder, TopicConsumer target)
+            => builder.Consumer == target
+                ? builder.Clone()
+                : new TopicBuilder(builder.TopicCollection, target);
     }
 }
