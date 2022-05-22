dc1e2a72d9bcf56e8170a3167df0ccf7cab0dfd6
pBouillon <pro.pierre.bouillon@protonmail.com>
2022-05-22 16:15:15 +0200

    Make the validation pipelines lazy

--- MqttTopicBuilder/Validators/ValidatorFactory.cs
+++ MqttTopicBuilder/Validators/ValidatorFactory.cs
@@ -1,4 +1,6 @@
-﻿using MqttTopicBuilder.Collection;
+using System;
+
+using MqttTopicBuilder.Collection;
 using MqttTopicBuilder.Validators.Rules.ITopicCollectionRules;
 using MqttTopicBuilder.Validators.Rules.RawTopicRules;
 
@@ -9,15 +11,36 @@ namespace MqttTopicBuilder.Validators;
 /// </summary>
 public static class ValidatorFactory
 {
+    private static readonly Lazy<Validator<string>> PublishedTopicValidator = new(() 
+        => Validator<string>.CreatePipelineWith(new MustNotContainWildcard()));
+
+    private static readonly Lazy<Validator<string>> RawTopicValidator = new(()
+        => Validator<string>.CreatePipelineWith(
+            new MustNotBeBlank(),
+            new MustEndWithMultiLevelWildcardIfAny(),
+            new MustHaveAtMostOneMultiLevelWildcard()));
+
+    private static readonly Lazy<Validator<string>> SingleRawTopicValidator = new(()
+        => Validator<string>.CreatePipelineWith(
+            new MustNotBeBlank(),
+            new MustBeUtf8(),
+            new MustNotHaveNullChar(),
+            new MustNotHaveSeparator(),
+            new MustRespectMaximumLength(),
+            new MustRespectWildcardsExclusivity()));
+
+    private static readonly Lazy<Validator<ITopicCollection>> TopicCollectionAppendingValidator = new(()
+        => Validator<ITopicCollection>.CreatePipelineWith(
+            new MustAppendingBeAllowed(),
+            new MustNotBeFull()));
+
+
     /// <summary>
-    /// Get a validator with a set of rule to validate that a topic
-    /// can be used on PUBLISH mode
+    /// Get a validator with a set of rule to validate that a topic can be used on PUBLISH mode
     /// </summary>
     /// <returns>The validator to be used</returns>
-    public static Validator<string> GetPublishedTopicValidator()
-        => Validator<string>
-            .CreatePipelineWith(
-                new MustNotContainWildcard());
+    public static Validator<string> GetPublishedTopicValidator() 
+        => PublishedTopicValidator.Value;
 
     /// <summary>
     /// Get a validator with a set of rules to validate a topic
@@ -27,25 +50,14 @@ public static class ValidatorFactory
     /// Does not ensure that the single topics from which the main topic is made of are valid
     /// </remarks>
     public static Validator<string> GetRawTopicValidator()
-        => Validator<string>
-            .CreatePipelineWith(
-                new MustNotBeBlank(),
-                new MustEndWithMultiLevelWildcardIfAny(),
-                new MustHaveAtMostOneMultiLevelWildcard());
+        => RawTopicValidator.Value;
 
     /// <summary>
     /// Get a validator with a set of rules to validate raw single topics
     /// </summary>
     /// <returns>The validator to be used</returns>
     public static Validator<string> GetSingleRawTopicValidator()
-        => Validator<string>
-            .CreatePipelineWith(
-                new MustNotBeBlank(),
-                new MustBeUtf8(),
-                new MustNotHaveNullChar(),
-                new MustNotHaveSeparator(),
-                new MustRespectMaximumLength(),
-                new MustRespectWildcardsExclusivity());
+        => SingleRawTopicValidator.Value;
 
     /// <summary>
     /// Get a validator with a set of rules to validate the ability for the collection
@@ -53,8 +65,5 @@ public static class ValidatorFactory
     /// </summary>
     /// <returns>The validator to be used</returns>
     public static Validator<ITopicCollection> GetTopicCollectionAppendingValidator()
-        => Validator<ITopicCollection>
-            .CreatePipelineWith(
-                new MustAppendingBeAllowed(),
-                new MustNotBeFull());
+        => TopicCollectionAppendingValidator.Value;
 }
